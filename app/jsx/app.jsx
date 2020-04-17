0fc55e3dd5650fa65fed451974e219e08cd4b7d6
Sean McGrath <smcgrath@hopper.com>
2020-04-17 18:10:25 -0400

    Set up SCSS boilerplate

--- app/jsx/app.jsx
+++ app/jsx/app.jsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import routes from './routes.jsx'
 import reducers from './reducers.jsx'
-// import '../styles/main.css'
+import '../styles/main.scss';
 
 const provider = <Provider store={createStore(reducers.reducer)}> {routes} </Provider>
 ReactDOM.render(provider, document.querySelector('.app'))
