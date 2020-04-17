929febc7b73b36dfc29e4282ff9f62866cc6efc6
Morgane Ciot <morganeciot@gmail.com>
2020-04-16 20:08:50 -0400

    Create Room component

---
+++ app/jsx/Room.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+export default class Room extends Component { 
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    async componentDidMount() {
+        // TODO need to disable the ability to hang up I think
+        try {
+            const domain = 'meet.jit.si'
+            const options = {
+                roomName: this.props.roomName,
+                height: 500,
+                parentNode: document.getElementById('jitsi-container'),
+                interfaceConfigOverwrite: {
+                    filmStripOnly: false,
+                    SHOW_JITSI_WATERMARK: false
+                },
+                configOverwrite: {
+                    disableSimulcast: false
+                }
+            }
+            const api = new window.JitsiMeetExternalAPI(domain, options)
+            api.addEventListener('videoConferenceJoined', () => {
+                api.executeCommand('displayName', this.props.displayName)
+            })
+        } catch (err) {
+            console.log('failed:', err)
+        }
+    }
+
+    render() {
+        return <div id="jitsi-container"></div>
+    }
+}
--- app/jsx/Vestibule.jsx
+++ app/jsx/Vestibule.jsx
@@ -2,6 +2,7 @@ import _ from 'lodash'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import reducers from './reducers.jsx'
+import Room from './Room.jsx'
 
 class Vestibule extends Component { 
     constructor(props) {
@@ -16,53 +17,16 @@ class Vestibule extends Component {
         this.state = {
 
         }
-
         console.log('jitsi api:', window.JitsiMeetExternalAPI)
     }
 
     async componentDidMount() {
         // Example usage of redux
         this.props.updateState('test')
-
-        try {
-            const domain = 'meet.jit.si'
-            const options = {
-                roomName: 'morgane',
-                height: 500,
-                parentNode: document.getElementById('jitsi-container'),
-                interfaceConfigOverwrite: {
-                    filmStripOnly: false,
-                    SHOW_JITSI_WATERMARK: false
-                },
-                configOverwrite: {
-                    disableSimulcast: false
-                }
-            }
-            const api = new window.JitsiMeetExternalAPI(domain, options)
-            api.addEventListener('videoConferenceJoined', () => {
-                console.log('user joined')
-                api.executeCommand('displayName', 'morg')
-            })
-        } catch (err) {
-            console.log('failed:', err)
-        }
     }
 
     render() {
-        return (
-            <div>
-                <ul className="room-list">
-                    {this.roomNames.map((name, index) => {
-                        return (
-                            <li key={index.toString()}>
-                                <a href={`https://meet.jit.si/${name}`}>{name}</a>
-                            </li>
-                        )
-                    })}
-                </ul>
-                <div id="jitsi-container"></div>
-            </div>
-        )
+        return <Room roomName='testjitsiroom' displayName='morg'></Room>
     }
 }
 
