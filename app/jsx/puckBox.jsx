a99672b5cf54c7f7dd0f99206bc48ae0a0da547d
danielmdavis <danis1911@gmail.com>
2020-04-24 17:03:23 -0400

    fixed inner box shifting

--- app/jsx/puckBox.jsx
+++ app/jsx/puckBox.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PuckBox = (props) => {
         return (
-            <div className='box' onClick={props.handleClick} onMouseOver={props.handleTouch}>
+            <div className={props.style} onClick={props.handleClick} onMouseOver={props.handleTouch}>
               <img className='image' src={props.image} />
             </div>
         )
