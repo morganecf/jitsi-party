b9e4750326e58eca5c7d39c944816ed00843eed9
danielmdavis <danis1911@gmail.com>
2020-04-24 18:18:19 -0400

    avatar selection response MVP. unhide and fix reselect button, and do more animation

--- app/jsx/PuckSelect.jsx
+++ app/jsx/PuckSelect.jsx
@@ -9,92 +9,103 @@ class PuckSelect extends Component {
         this.state = {
             columnOpen: false,
             rowOpen: null,
+            avatar: null
         }
         this.onButtonClick = this.onButtonClick.bind(this)
       }
 
   onButtonClick() { this.setState({ columnOpen: true }) }
   handleClick(id) { this.setState({ rowOpen: id }) }
-  // handleClickColor(id) { this.setState({ avatar: [this.state.rowOpen,id] }) }
-  handleClickColor(id) { this.props.handleSelect([this.state.rowOpen,id])  }
+  handleClickColor(id) {
+    this.setState({ avatar: id })
+    this.props.handleSelect([this.state.rowOpen,id])
+  }
 
   render() {
 
-
-    let puck_list = avatars.map(function(arr,i) { return arr[0] })
-
-
-    let puck_array
-
     //
-    let shown = { visibility: 'visible' }
+    let button_hide = { visibility: 'visible' }
 
-    if (this.state.columnOpen) {
-      //
-      shown = { visibility: 'hidden' }
+    if (this.state.avatar) {
+      console.log(this.state.avatar)
 
-      puck_array = puck_list.map((puck,id) => {
+      //<input style={button_hide} type="button" onClick={this.onButtonClick} value="Reselect?"/>
+      return (
+        <div className='outer'>
+          <div><img src={avatars[this.state.rowOpen][this.state.avatar]}/></div>
+        </div>
+      )
 
-        if (this.state.rowOpen===id) {
-
-          // let handleClick = () => this.handleClick(puck_list[id])
-
-          //
+    } else {
 
 
-          let variant_array = avatars[this.state.rowOpen].map((color,id) => {
+      let puck_list = avatars.map(function(arr,i) { return arr[0] })
+      let puck_array
+      if (this.state.columnOpen) {
+        //
+        button_hide = { visibility: 'hidden' }
+        puck_array = puck_list.map((puck,id) => {
 
-            let selected = 'box'
-            // if (id===0) { selected = 'box-selected' }
-            let handleClickColor = () => this.handleClickColor(id)
+          if (this.state.rowOpen===id) {
 
-            // avatar color variant selection
+            let variant_array = avatars[this.state.rowOpen].map((color,id) => {
+
+              let selected = 'box'
+              // if (id===0) { selected = 'box-selected' }
+              let handleClickColor = () => this.handleClickColor(id)
+
+              // avatar color variant selection
+              return (
+                <PuckBox
+                handleClick={handleClickColor}
+                key={id}
+                image={color}
+                style={selected}
+                />
+              )
+            })
+
+            return (
+              <div className='inner'>
+              {variant_array}
+              </div>
+            )
+
+          } else {
+
+            let handleClick = () => this.handleClick(id)
+
+            let image_transparency = 'image'
+            if (this.state.rowOpen && this.state.rowOpen!==id) {
+              image_transparency = 'non-selected-image'
+            }
+            // avatar style selection
             return (
               <PuckBox
-              handleClick={handleClickColor}
+              handleClick={handleClick}
               key={id}
-              image={color}
-              style={selected}
+              image={puck}
+              style='box'
+              imageStyle={image_transparency}
               />
             )
-          })
 
-          return (
-            <div className='inner'>
-              {variant_array}
-            </div>
-          )
-
-        } else {
-
-          let handleClick = () => this.handleClick(id)
-
-          let image_transparency = 'image'
-          if (this.state.rowOpen && this.state.rowOpen!==id) {
-            image_transparency = 'non-selected-image'
           }
-          // avatar style selection
-          return (
-            <PuckBox
-            handleClick={handleClick}
-            key={id}
-            image={puck}
-            style='box'
-            imageStyle={image_transparency}
-            />
-          )
 
-        }
+        })
+      }
 
-      })
+      return(
+        <div className='outer'>
+        <input style={button_hide} type="button" onClick={this.onButtonClick} value="Avatar?"/>
+        {puck_array}
+        </div>
+      )
+
     }
 
-    return(
-      <div className='outer'>
-        <input style={shown} type="button" onClick={this.onButtonClick} value="Avatar?"/>
-        {puck_array}
-      </div>
-    )
+
+
   }
 }
 export default PuckSelect
