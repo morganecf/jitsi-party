dc9457724c4210aaf1d261ccca9dd6dc29d30ad4
danielmdavis <danis1911@gmail.com>
2020-04-25 18:18:04 -0400

    the pucks are in there

--- app/client/jsx/avatars.jsx
+++ app/client/jsx/avatars.jsx
@@ -1,7 +1,8 @@
 export default
 [
+  ['./js/images/puck/normal/normal1.png','./js/images/puck/normal/normal2.png','./js/images/puck/normal/normal3.png','./js/images/puck/normal/normal4.png',],
+  ['./js/images/puck/book/book1.png','./js/images/puck/book/book2.png','./js/images/puck/book/book3.png','./js/images/puck/book/book4.png',],
+  ['./js/images/puck/whippit/whippit1.png','./js/images/puck/whippit/whippit2.png','./js/images/puck/whippit/whippit3.png','./js/images/puck/whippit/whippit4.png',],
   ['./js/images/puck/dancing/dancing1.png','./js/images/puck/dancing/dancing2.png','./js/images/puck/dancing/dancing3.png','./js/images/puck/dancing/dancing4.png',],
-  ['./js/images/puck/dancing/dancing1.png','./js/images/puck/dancing/dancing2.png','./js/images/puck/dancing/dancing3.png','./js/images/puck/dancing/dancing4.png',],
-  ['./js/images/puck/dancing/dancing1.png','./js/images/puck/dancing/dancing2.png','./js/images/puck/dancing/dancing3.png','./js/images/puck/dancing/dancing4.png',],
-  ['./js/images/puck/dancing/dancing1.png','./js/images/puck/dancing/dancing2.png','./js/images/puck/dancing/dancing3.png','./js/images/puck/dancing/dancing4.png',]
+  ['./js/images/puck/trippy/trippy1.png','./js/images/puck/trippy/trippy2.png','./js/images/puck/trippy/trippy3.png','./js/images/puck/trippy/trippy4.png',]
 ]
