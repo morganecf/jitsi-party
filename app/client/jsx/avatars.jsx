16467f22324befd332a06b638e4088cbe569783e
danielmdavis <danis1911@gmail.com>
2020-04-25 19:37:07 -0400

    thumb puck

--- app/client/jsx/avatars.jsx
+++ app/client/jsx/avatars.jsx
@@ -4,5 +4,6 @@ export default
   ['./js/images/puck/book/book1.png','./js/images/puck/book/book2.png','./js/images/puck/book/book3.png','./js/images/puck/book/book4.png',],
   ['./js/images/puck/whippit/whippit1.png','./js/images/puck/whippit/whippit2.png','./js/images/puck/whippit/whippit3.png','./js/images/puck/whippit/whippit4.png',],
   ['./js/images/puck/dancing/dancing1.png','./js/images/puck/dancing/dancing2.png','./js/images/puck/dancing/dancing3.png','./js/images/puck/dancing/dancing4.png',],
-  ['./js/images/puck/trippy/trippy1.png','./js/images/puck/trippy/trippy2.png','./js/images/puck/trippy/trippy3.png','./js/images/puck/trippy/trippy4.png',]
+  ['./js/images/puck/trippy/trippy1.png','./js/images/puck/trippy/trippy2.png','./js/images/puck/trippy/trippy3.png','./js/images/puck/trippy/trippy4.png',],
+  ['./js/images/puck/thumb/thumb1.png','./js/images/puck/thumb/thumb2.png','./js/images/puck/thumb/thumb3.png','./js/images/puck/thumb/thumb4.png',]
 ]
