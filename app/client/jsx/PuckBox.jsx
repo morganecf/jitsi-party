54015f7acb06ccdc0d824278ffc35ebbe651741b
danielmdavis <danis1911@gmail.com>
2020-04-25 18:18:04 -0400

    the pucks are in there

--- app/client/jsx/PuckBox.jsx
+++ app/client/jsx/PuckBox.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const PuckBox = (props) => {
   return (
     <div className={props.style} onClick={props.handleClick} onMouseOver={props.handleTouch}>
-      <img className='image' src={props.image} className={props.imageStyle} />
+      <img className='image' src={props.image}  />
     </div>
   )
 }
