fbb1e9a3f79e795f6c7852c2bc1b89335d196354
Morgane Ciot <morganeciot@gmail.com>
2020-04-25 14:58:54 -0400

    Persist video/audio settings

--- app/client/jsx/Room.jsx
+++ app/client/jsx/Room.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Redirect } from 'react-router-dom'
 import reducers from './reducers.jsx'
+import { Redirect } from 'react-router-dom'
 import RoomLayout from './RoomLayout.jsx'
 import JitsiVideo from './JitsiVideo.jsx'
 import ArtRoom from './ArtRoom.jsx'
