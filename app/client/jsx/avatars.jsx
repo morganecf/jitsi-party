870cd0af31a85fc329c2971a1f25e46c47c0e817
danielmdavis <danis1911@gmail.com>
2020-04-30 17:32:22 -0400

    fonts

--- app/client/jsx/avatars.jsx
+++ app/client/jsx/avatars.jsx
@@ -7,3 +7,45 @@ export default
   ['./js/images/puck/trippy/trippy1.png','./js/images/puck/trippy/trippy2.png','./js/images/puck/trippy/trippy3.png','./js/images/puck/trippy/trippy4.png',],
   ['./js/images/puck/thumb/thumb1.png','./js/images/puck/thumb/thumb2.png','./js/images/puck/thumb/thumb3.png','./js/images/puck/thumb/thumb4.png',]
 ]
+
+const relativePath = './js/images/puck/'
+const hostedPath = 'https://raw.githubusercontent.com/morganecf/jitsi-party/master/app/client/js/images/puck/'
+
+const avatars = {
+  normal: {
+    purple: { name: 'normal-purple.png' },
+    blue: { name: 'normal-blue.png' },
+    red: { name: 'normal-red.png' },
+    orange: { name: 'normal-orange.png' },
+  },
+  book: {
+    purple: { name: 'book-purple.png' },
+    blue: { name: 'book-blue.png' },
+    red: { name: 'book-red.png' },
+    orange: { name: 'book-orange.png' },
+  },
+  whippit: {
+    purple: { name: 'whippit-purple.png' },
+    blue: { name: 'whippit-blue.png' },
+    red: { name: 'whippit-red.png' },
+    orange: { name: 'whippit-orange.png' },
+  },
+  dancing: {
+    purple: { name: 'dancing-purple.png' },
+    blue: { name: 'dancing-blue.png' },
+    red: { name: 'dancing-red.png' },
+    orange: { name: 'dancing-orange.png' },
+  },
+  trippy: {
+    purple: { name: 'trippy-purple.png' },
+    blue: { name: 'trippy-blue.png' },
+    red: { name: 'trippy-red.png' },
+    orange: { name: 'trippy-orange.png' },
+  },
+  thumb: {
+    purple: { name: 'thumb-purple.png' },
+    blue: { name: 'thumb-blue.png' },
+    red: { name: 'thumb-red.png' },
+    orange: { name: 'thumb-orange.png' },
+  },
+}
