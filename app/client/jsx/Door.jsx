49fa3c04e0c1778ecebd7cffca05b80301075162
Morgane Ciot <morganeciot@gmail.com>
2020-05-02 18:24:56 -0400

    Enforce room capacity

--- app/client/jsx/Door.jsx
+++ app/client/jsx/Door.jsx
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import React, { Component } from 'react'
 import UserList from './UserList.jsx';
 
@@ -11,22 +12,24 @@ class Door extends Component {
             `${this.props.users.length} people are in this room!` :
             '1 person is in this room!'
         const users = this.props.users.length ?
-            (
+        (
                 <div className="user-list">
                     <div>{userStr}</div>
-                    <UserList users={this.props.users} truncate={true}></UserList>
+                    <UserList users={this.props.users} truncate={true} room={this.props.room}></UserList>
                 </div>
             ) :
             <div>No one is in this room :(</div>
 
         const tintStyle = this.props.tintColor ? { background: this.props.tintColor } : {}
+        const isAtCapacity = this.props.room.capacity && this.props.users.length >= this.props.room.capacity
+        const buttonText = isAtCapacity ? 'Room is full' : `Enter ${this.props.room.name}`
 
         return (
             <div className="door-wrapper">
                 <div className="door-background"></div>
                 <div className="door-target">
                     {users}
-                    <button onClick={this.props.onClick.bind(this)}>Enter {this.props.room.name}</button>
+                    <button disabled={isAtCapacity} onClick={this.props.onClick.bind(this)}>{buttonText}</button>
                 </div>
                 <div id="door" className="door">
                     <div className="tint" style={tintStyle}></div>
