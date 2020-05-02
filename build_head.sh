e2eb3c69cf2fbfb408a594941be793767d5ae764
Andrew Hall <hndrewaall@gmail.com>
2020-05-02 01:09:22 -0700

    Lots of prod fixes

---
+++ build_head.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+
+SCRIPT=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT")
+
+pushd "$SCRIPT_DIR"
+
+DOCKER_REPO="localhost:5000/gbre/jitsi-party"
+
+QUERY='{ repository(owner: \"morganecf\", name: \"jitsi-party\") { releases(last: 1, orderBy: {field: CREATED_AT, direction: ASC}) {edges { node { tagName }}}}}'
+
+pushd app/client
+
+npm install
+node_modules/.bin/webpack
+rm -rf node_modules
+
+popd
+
+docker build . --tag "$DOCKER_REPO":latest
+docker push "$DOCKER_REPO":latest
+
+popd
