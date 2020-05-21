ac0a83fae81d10f17aa99fee7b199911a737079b
Sean McGrath <smcgrath@hopper.com>
2020-05-21 16:10:19 -0400

    better handling for goodbye avatar

--- app/client/jsx/Exit.jsx
+++ app/client/jsx/Exit.jsx
@@ -8,10 +8,19 @@ class Exit extends Component {
     }
 
     render() {
+        // Support either a single "goodbye" avatar or a map of color -> image
+        const goodbyeImgUrl =
+            (Config.specialAvatars && Config.specialAvatars.goodbye)
+                ? (Config.specialAvatars.goodbye[this.props.user.avatar.color] || Config.specialAvatars.goodbye)
+                : null
+
+        const goodbyeImg = goodbyeImgUrl
+            ? <img src={goodbyeImgUrl}/>
+            : null
         return (
             <div className="exit">
                 <h1>Goodbye!</h1>
-                <img src={Config.specialAvatars.thumb[this.props.user.avatar.color]}/>
+                {goodbyeImg}
             </div>
         )
     }
