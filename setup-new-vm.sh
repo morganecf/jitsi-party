dcc80cdc4e6e8a611b25da7093c6a4695b8e4ccd
bMacSwigg <brian.mcswiggen@gmail.com>
2020-06-09 20:32:27 -0400

    Update start scripts

---
+++ setup-new-vm.sh
@@ -0,0 +1,12 @@
+# get git
+sudo apt-get install git
+
+# get pip for Python3
+sudo apt-get install python3-pip
+
+# get *proper* nodeJS version
+sudo curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+sudo apt-get install -y nodejs
+
+# For checking on DNS config (not necessary)
+# sudo apt-get install dnsutils
