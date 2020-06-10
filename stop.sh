45160d4e00a21b2a56af5d8610b5b4f32647b198
bMacSwigg <brian.mcswiggen@gmail.com>
2020-06-09 22:00:30 -0400

    Fix pkills

--- stop.sh
+++ stop.sh
@@ -2,7 +2,14 @@
 sudo cp ~/jitsi-party/landing/down.html /var/www/html/index.html
 
 # Shut down flask
-pkill "flask"
+pkill -x "flask"
 
-# Shut down run.sh
-pkill "run.sh"
+# Shut down `bash run.sh`
+pkill -x "bash"
+
+# shut down webpack
+pkill -x "sh"
+
+# shut down node... and reset, because this breaks tty for some reason
+pkill -x "node"
+reset
