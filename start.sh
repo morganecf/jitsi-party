b5582f00bbb2fb335db0387d2c31152559a8ff54
Brian McSwiggen <brian.mcswiggen@gmail.com>
2020-07-21 23:05:42 +0000

    change start port to not conflict with landing page

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ export FLASK_APP="manager.py"
 export FLASK_ENV=production
 export FLASK_DEBUG=1
 export PYTHONUNBUFFERED=1
-export FLASK_RUN_PORT=80
+export FLASK_RUN_PORT=3000
 flask run-eventlet
 
