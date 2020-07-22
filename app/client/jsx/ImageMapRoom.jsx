bea4ac763ba516fc52785a4df9029830e248e89e
bMacSwigg <brian.mcswiggen@gmail.com>
2020-07-21 22:07:56 -0400

    Make functional component

--- app/client/jsx/ImageMapRoom.jsx
+++ app/client/jsx/ImageMapRoom.jsx
@@ -1,33 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class ImageMapRoom extends Component {
-
-    getAreasHtml() {
-        return this.props.imageMapOptions.areas.map((a,i) =>
-            <area
-                key={i}
-                className="imagemap-area"
-                shape={a.shape}
-                coords={a.coords}
-                alt={a.label}
-                title={a.label}
-                // This is just a dummy onClick, to show interaction with the area
-                onClick={() => console.log(a.label)}
-            />
-        );
-    }
-
-    render() {
-        return (
-            <div className="imagemap-room">
-                <img src={this.props.imageMapOptions.img} useMap="#imgmap" />
-
-                <map name="imgmap">
-                    {this.getAreasHtml()}
-                </map>
-            </div>
-        )
-    }
+function getAreasHtml(areasList) {
+    return areasList.map((a,i) =>
+        <area
+            key={i}
+            className="imagemap-area"
+            shape={a.shape}
+            coords={a.coords}
+            alt={a.label}
+            title={a.label}
+            // This is just a dummy onClick, to show interaction with the area
+            onClick={() => console.log(a.label)}
+        />
+    );
 }
 
-export default ImageMapRoom;
+export default props => {
+    return (
+        <div className="imagemap-room">
+            <img src={props.imageMapOptions.img} useMap="#imgmap" />
+
+            <map name="imgmap">
+                {this.getAreasHtml(props.imageMapOptions.areas)}
+            </map>
+        </div>
+    )
+}
