6ac4ea9c443c14ab1dcd798952061c51e0acacee
bMacSwigg <brian.mcswiggen@gmail.com>
2020-07-21 21:24:14 -0400

    Reformat LibraryRoom

--- app/client/jsx/LibraryRoom.jsx
+++ app/client/jsx/LibraryRoom.jsx
@@ -4,20 +4,25 @@ class LibraryRoom extends Component {
 
     getAreasHtml() {
         return this.props.libOptions.areas.map((a,i) =>
-            <area key={i} className="library-topic" shape={a.shape} coords={a.coords} alt={a.category} onClick={console.log(a.category)} />
+            <area
+                key={i}
+                className="library-topic"
+                shape={a.shape}
+                coords={a.coords}
+                alt={a.category}
+                title={a.category}
+                onClick={console.log(a.category)} />
         );
     }
 
     render() {
         return (
             <div className="library-room">
-                <div className="library-map">
-                    <img src={this.props.libOptions.img} useMap="#libmap" />
+                <img src={this.props.libOptions.img} useMap="#libmap" />
 
-                    <map name="libmap">
-                        {this.getAreasHtml()}
-                    </map>
-                </div>
+                <map name="libmap">
+                    {this.getAreasHtml()}
+                </map>
             </div>
         )
     }
