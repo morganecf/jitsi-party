d137c9dbcd373e6328e4a9837dee6c48af4e12ed
morganecf <morganeciot@gmail.com>
2020-07-21 20:10:05 -0400

    Modify PuckSelect and add background

--- app/client/jsx/PuckSelect.jsx
+++ app/client/jsx/PuckSelect.jsx
@@ -35,6 +35,17 @@ class PuckSelect extends Component {
     })
   }
 
+  handleClickAvatar(avatarDesign, variantKey) {
+    this.setState({
+      avatarDesign, 
+      avatarColorway: variantKey
+    })
+    this.props.handleSelect({
+      type: avatarDesign,
+      color: variantKey
+    })
+  }
+
   render () {
     const avatars = Config.avatars
     if (this.state.avatarColorway === '') { // checks whether selection not yet complete
@@ -44,41 +55,42 @@ class PuckSelect extends Component {
       if (this.state.columnOpen) {
         fade = 'fade'
         allAvatarDesigns = Object.keys(avatars).map((key, index) => {
-          if (this.state.rowOpen !== index) { // checks each item in column for whether selected
-            let imageTransparency = 'image'
-            if (this.state.rowOpen !== null && this.state.rowOpen !== index) {
-              imageTransparency = 'non-selected-image'
-            }
-            const handleClick = () => this.handleClickDesign(key, index)
+          // if (this.state.rowOpen !== index) { // checks each item in column for whether selected
+          //   let imageTransparency = 'image'
+          //   if (this.state.rowOpen !== null && this.state.rowOpen !== index) {
+          //     imageTransparency = 'non-selected-image'
+          //   }
+          //   const handleClick = () => this.handleClickDesign(key, index)
+          //   return (
+          //     <PuckBox
+          //       handleClick={handleClick}
+          //       key={key}
+          //       image={avatars[key][Object.keys(avatars[key])[0]]}
+          //       style='box'
+          //       imageStyle={imageTransparency}
+          //     />
+          //   )
+          // } else { // selected design
+          allColorwayVariants = Object.keys(avatars[key]).map((variantKey, colorIndex) => {
+            const selected = 'image'
+            // const handleClick = () => this.handleClickColor(variantKey, colorIndex)
+            const handleClick = () => this.handleClickAvatar(key, variantKey)
             return (
               <PuckBox
-                handleClick={handleClick}
-                key={key}
-                image={avatars[key][Object.keys(avatars[key])[0]]}
-                style='box'
-                imageStyle={imageTransparency}
+                handleClick={handleClick.bind(this)}
+                key={`${key} ${variantKey}`}
+                image={avatars[key][variantKey]}
+                imageStyle={selected}
               />
             )
-          } else { // selected design
-            allColorwayVariants = Object.keys(avatars[key]).map((variantKey, colorIndex) => {
-              const selected = 'image'
-              const handleClick = () => this.handleClickColor(variantKey, colorIndex)
-              return (
-                <PuckBox
-                  handleClick={handleClick.bind(this)}
-                  key={`${key} ${variantKey}`}
-                  image={avatars[key][variantKey]}
-                  imageStyle={selected}
-                />
-              )
-            })
-            return ( // colorways row in place of selected column item
-              <div key='big-container' className='inner'>
-                {allColorwayVariants}
-              </div>
-            )
-          }
+          })
+          return ( // colorways row in place of selected column item
+            <div key={`big-container ${key}`} className='inner'>
+              {allColorwayVariants}
+            </div>
+          )
         })
+        // })
       }
       return (
         <div className='outer'>
@@ -89,7 +101,7 @@ class PuckSelect extends Component {
     } else {
       return (
         <div className='outer'>
-          <input className='fade opaque' type='button' onClick={this.handleButtonClickReset} value={Config.welcomePage.avatarSelectionText} />
+          <input className='fade opaque' type='button' onClick={this.handleButtonClickReset.bind(this)} value={Config.welcomePage.avatarSelectionText} />
           <div className='spacer' />
           <div><img className='image' src={avatars[this.state.avatarDesign][this.state.avatarColorway]} /></div>
         </div>
