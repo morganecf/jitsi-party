90d4c35e56a32e0b3e04f6146427a7294fae4941
Brian McSwiggen <brian.mcswiggen@gmail.com>
2020-07-23 00:33:11 +0000

    add useState which I forgot

--- app/client/jsx/ImageMapContents.jsx
+++ app/client/jsx/ImageMapContents.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
