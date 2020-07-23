3a04ff0b281961f9fb77309ba9a0810ff4af3f45
Morg <morgane@datarobot.com>
2020-07-23 14:50:10 -0400

    Add flask command to generate imagemap config for convenience (#156)
    * Add flask command to generate imagemap config for convenience
    
    * remove reading room

--- app/manager.py
+++ app/manager.py
@@ -10,8 +10,8 @@ overridedir = os.path.join(configdir, 'overrides')
 
 # Create the application context
 app = create_app(os.getenv('FLASK_ENV') or 'default')
-# The following are custom flask commands
 
+# The following are custom flask commands
 
 @app.shell_context_processor
 def make_shell_context():
@@ -49,3 +49,57 @@ def create_db():
                 for room_name, room in roomConfig.items()]
     db.session.add_all(db_rooms)
     db.session.commit()
+
+
+@app.cli.command('create-imagemap-config')
+def create_imagemap_config():
+    '''Usage: flask create-imagemap-config
+
+    Running this will generate a config file based on the contents of js/imagemap-content. Each subdirectory
+    corresponds to an HTML image map, and each subdirectory within that corresponds to an area of the map.
+    Some values are filled with placeholders and are left up to the user to fill in. This simply outputs the
+    json, letting the user pipe it to their config file of choice.
+    '''
+    def listdir(dir):
+        return filter(lambda i: not i.startswith('.'), os.listdir(dir))
+    
+    def format_item_path(imagemap_dir, topic, item):
+        return os.path.join('js', 'imagemap-content', imagemap_dir, topic, item)
+
+    basedir = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)),
+        'client',
+        'js',
+        'imagemap-content'
+    )
+
+    if not os.path.exists(basedir):
+        print('There is no image map content')
+        return
+
+    config = {}
+    imagemaps = listdir(basedir)
+
+    for imagemap in imagemaps:
+        topic_dir = os.path.join(basedir, imagemap)
+        topics = listdir(topic_dir)
+        areas = [{
+            'shape': '<AREA SHAPE:str>',
+            'coords': '<AREA COORDS:str>',
+            'label': topic,
+            'contents': [
+                {
+                    'title': '',
+                    'path': format_item_path(imagemap, topic, item)
+                } for item in listdir(os.path.join(topic_dir, topic))
+            ]
+        } for topic in topics]
+
+        config[imagemap] = {
+            'img': '<PATH TO IMAGE:str>',
+            'areas': areas
+        }
+
+    print(json.dumps({ 'imagemaps': config }, indent=4))
+
+
