5ed6b889dba20d697c7f4d6955d560538e497e26
Morg <morgane@datarobot.com>
2020-07-24 13:12:09 -0400

    TST Event config (#162)
    * Modify PuckSelect and add background
    
    * First pass at styles, event config
    
    * Map stats
    
    * Update stats
    
    * Change accent color
    
    * Email form colors
    
    * Rename TST config files
    
    * Event description colors
    
    * Update events
    
    * Start working on rooms
    
    * Rooms and map
    
    * Adventure
    
    * Madame name
    
    * Add image map library
    
    * Add PDF content
    
    * Remove pdfs
    
    * Generate imagemap config
    
    * Finish library
    
    * Mod email and iframe rooms
    
    * Embed Movie & Chat
    
    * Reorganize map
    
    * Fix tearoom/kitchen switch
    
    * Update events
    
    * Image maps
    
    * Touchups
    
    * font swap
    
    * Fix font and puckbox
    
    Co-authored-by: danielmdavis <danis1911@gmail.com>

--- app/client/jsx/IFrameRoom.jsx
+++ app/client/jsx/IFrameRoom.jsx
@@ -2,10 +2,22 @@ import React from 'react'
 import JitsiVideo from './JitsiVideo.jsx'
 
 export default props => {
-    const { src } = props.iframeOptions
+    /* secondSrc is TEMPORARY TST-SPECIFIC THING */
+    const { src, secondSrc } = props.iframeOptions
+    const left = secondSrc ? (
+        <div className="iframe-section">
+                <iframe
+                    src={secondSrc}
+                    height="100%"
+                    width="100%"
+                    frameBorder="0px"
+                >
+                </iframe>
+            </div>
+    ) : <JitsiVideo jitsiData={props.jitsiData}></JitsiVideo>
     return (
         <div className="iframe-room">
-            <JitsiVideo jitsiData={props.jitsiData}></JitsiVideo>
+            {left}
             <div className="iframe-section">
                 <iframe
                     src={src}
