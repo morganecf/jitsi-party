101215c92aa911d08b04befcba300c096f616c42
Morg <morgane@datarobot.com>
2020-07-25 17:13:08 -0400

    LAST MINUTE (#173)
    * TST TV fix
    
    * Use subprocess bc twilio api uses ssl
    
    * Rename TST_Chat

--- app/client/jsx/IFrameRoom.jsx
+++ app/client/jsx/IFrameRoom.jsx
@@ -2,22 +2,10 @@ import React from 'react'
 import JitsiVideo from './JitsiVideo.jsx'
 
 export default props => {
-    /* secondSrc is TEMPORARY TST-SPECIFIC THING */
-    const { src, secondSrc } = props.iframeOptions
-    const left = secondSrc ? (
-        <div className="iframe-section">
-                <iframe
-                    src={secondSrc}
-                    height="100%"
-                    width="100%"
-                    frameBorder="0px"
-                >
-                </iframe>
-            </div>
-    ) : <JitsiVideo jitsiData={props.jitsiData}></JitsiVideo>
+    const { src, showVideo } = props.iframeOptions
     return (
         <div className="iframe-room">
-            {left}
+            {showVideo && <JitsiVideo jitsiData={props.jitsiData}></JitsiVideo>}
             <div className="iframe-section">
                 <iframe
                     src={src}
