2311fa48663fdf121ee712f5fc9b2babec51d629
Brian McSwiggen <brian.mcswiggen@gmail.com>
2020-07-25 03:55:58 +0000

    resize imagemap dynamically to fit the screen

--- app/client/jsx/ImageMapRoom.jsx
+++ app/client/jsx/ImageMapRoom.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import Modal from 'react-modal'
 import ImageMapper from 'react-image-mapper'
 import ImageMapContents from './ImageMapContents.jsx'
@@ -19,16 +19,51 @@ function onDisplayModal(areasList, setIsShown, setArea) {
     }
 }
 
+// Calculate the full width that can fit in the space, given the
+// bounding box defined by (maxWidth, maxHeight) and the aspect ratio
+// defined by (curWidth, curHeight)
+function calculateFullWidth(maxWidth, maxHeight, curWidth, curHeight) {
+    console.log(maxWidth, maxHeight, curWidth, curHeight)
+    var ratio = curWidth / curHeight;
+    var maxWidthFromHeight = maxHeight * ratio;
+    return Math.floor(Math.min(maxWidth, maxWidthFromHeight));
+}
+
 export default props => {
     const [isShown, setIsShown] = useState(false)
     const [area, setArea] = useState({})
+
+    // Get the width of the actual image file
+    const [imgSize, setImgSize] = useState({})
+    var img = new Image()
+    img.onload = () =>
+        setImgSize({"width":img.naturalWidth, "height":img.naturalHeight})
+    img.src = props.imageMapOptions.img
+
+    // Get the max width supported by the room
+    const [maxWidth, setMaxWidth] = useState(0)
+    const divRef = useRef(null)
+    useEffect(() => {
+        var maxW =
+            calculateFullWidth(
+                divRef.current.clientWidth,
+                divRef.current.clientHeight,
+                imgSize.width,
+                imgSize.height)
+        if (!isNaN(maxW) && maxW !== maxWidth) {
+            setMaxWidth(maxW)
+        }
+    }, [divRef.current && divRef.current.clientWidth, imgSize.width])
+
     return (
-        <div className="imagemap-room">
+        <div className="imagemap-room" ref={divRef}>
             <ImageMapper
                 src={props.imageMapOptions.img}
                 map={{"name":"img", "areas":formatAreas(props.imageMapOptions.areas)}}
                 fillColor="rgba(255,255,255,0.25)"
                 onClick={onDisplayModal(props.imageMapOptions.areas, setIsShown, setArea)}
+                imgWidth={imgSize.width}
+                width={maxWidth}
             />
 
             <Modal
