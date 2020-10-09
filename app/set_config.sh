e2267cdd7b5e0e029bf3d5f1b646ee301c889cbe
morganecf <morganeciot@gmail.com>
2020-10-08 22:06:29 -0400

    Hardlink config instead of symlinking because `require` does not seem to understand symlinks the canonical way

--- app/set_config.sh
+++ app/set_config.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Usage: ./set_config.sh <name of config directory>
-#   This symlinks the config based on the name of the config
+#   This hardlinks the config based on the name of the config
 #   directory.
 #   Example: ./set_config.sh cabin-weekend
 
 if [ -d config/$1 ]; then
     rm -f config/overrides/*
-    ln -s config/$1/* config/overrides/
+    ln config/$1/* config/overrides/
     flask create-db
     echo "${1} has been set as the current config."
 else
