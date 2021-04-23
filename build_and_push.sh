88de1c925c8afa356d2a474c3d99a20521082724
Andrew Hall <hndrewaall@gmail.com>
2021-04-23 19:19:17 -0400

    Fix random build issues

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
+set -xe
+
 make webpack
 
-PARTY_CONFIG=`readlink app/config/overrides`
+PARTY_CONFIG=`readlink app/config/overrides` || echo "No config set, using base"
 PARTY_CONFIG="${PARTY_CONFIG:-base}"
 
 PARTY_THEME_FILE=`readlink app/client/styles/themes/_active.scss`
@@ -15,24 +17,24 @@ GIT_COMMIT=`git rev-parse HEAD`
 REPO_BASE="cabinweekend/jitsi-party"
 
 export DOCKER_BUILDKIT=1
-docker-compose build web-worker_1
-docker-compose push web-worker_1
+docker-compose build
+docker-compose push
 
 docker tag $REPO_BASE-web:latest $REPO_BASE-web:$PARTY_CONFIG-$PARTY_THEME-$GIT_COMMIT
 docker tag $REPO_BASE-web:latest $REPO_BASE-web:$PARTY_CONFIG-$PARTY_THEME-latest
 docker push $REPO_BASE-web:$PARTY_CONFIG-$PARTY_THEME-$GIT_COMMIT
 docker push $REPO_BASE-web:$PARTY_CONFIG-$PARTY_THEME-latest
 
-#docker tag $REPO_BASE-proxy:latest $REPO_BASE-proxy:$GIT_COMMIT
-#docker push $REPO_BASE-proxy:$GIT_COMMIT
-#
-#docker tag $REPO_BASE-prosody:latest $REPO_BASE-prosody:$GIT_COMMIT
-#docker push $REPO_BASE-prosody:$GIT_COMMIT
-#
-#docker tag $REPO_BASE-node:latest $REPO_BASE-node:$GIT_COMMIT
-#docker push $REPO_BASE-node:$GIT_COMMIT
-#
-#docker tag $REPO_BASE-meet:latest $REPO_BASE-meet:$GIT_COMMIT
-#docker push $REPO_BASE-meet:$GIT_COMMIT
+docker tag $REPO_BASE-proxy:latest $REPO_BASE-proxy:$GIT_COMMIT
+docker push $REPO_BASE-proxy:$GIT_COMMIT
+
+docker tag $REPO_BASE-prosody:latest $REPO_BASE-prosody:$GIT_COMMIT
+docker push $REPO_BASE-prosody:$GIT_COMMIT
+
+docker tag $REPO_BASE-node:latest $REPO_BASE-node:$GIT_COMMIT
+docker push $REPO_BASE-node:$GIT_COMMIT
+
+docker tag $REPO_BASE-meet:latest $REPO_BASE-meet:$GIT_COMMIT
+docker push $REPO_BASE-meet:$GIT_COMMIT
 
 
