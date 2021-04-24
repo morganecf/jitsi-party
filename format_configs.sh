b8cb89d3ec25c8f6f2be90baaa97598e8424130b
Andrew Hall <hndrewaall@gmail.com>
2021-04-24 00:39:03 -0400

    Revert map and fix formatter
    Also drop old adventure

--- format_configs.sh
+++ format_configs.sh
@@ -2,7 +2,7 @@
 
 for f (app/config/**/*json); do
     g=${f//json/json.old}
-    mv $f $g
-    python3 -mjson.tool $g > $f || echo "Formatting failed for $f!"
-    rm $g
+    cp $f $g
+    (python3 -mjson.tool $g > $f && rm $g) || \
+        (echo "Formatting failed for $f!" && rm $f && mv $g $f)
 done
