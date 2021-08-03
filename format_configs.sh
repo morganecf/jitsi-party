7348c3c05ba17901c74169294781a6e587f27388
Andrew Hall <hndrewaall@gmail.com>
2021-08-03 19:35:42 -0400

    Fix script

--- format_configs.sh
+++ format_configs.sh
@@ -3,6 +3,10 @@
 for f (app/config/**/*json); do
     g=${f//json/json.old}
     cp $f $g
-    (python3 -mjson.tool $g > $f && rm $g) || \
-        (echo "Formatting failed for $f!" && rm $f && mv $g $f)
+    if ! (python3 -mjson.tool $g > $f && rm $g); then
+        echo "Formatting failed for $f!"
+        rm $f
+        mv $g $f
+        exit 1
+    fi
 done
