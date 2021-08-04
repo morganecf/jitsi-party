968cfabe6ee1724ae9ae2fba73f4006b98599b58
Andrew Hall <hndrewaall@gmail.com>
2021-08-03 20:00:57 -0400

    Automatically test configs with GHA (#612)

--- format_configs.sh
+++ format_configs.sh
@@ -1,8 +1,12 @@
-#! /bin/zsh
+#! /usr/bin/env zsh
 
 for f (app/config/**/*json); do
     g=${f//json/json.old}
     cp $f $g
-    (python3 -mjson.tool $g > $f && rm $g) || \
-        (echo "Formatting failed for $f!" && rm $f && mv $g $f)
+    if ! (python3 -mjson.tool $g > $f && rm $g); then
+        echo "Formatting failed for $f!"
+        rm $f
+        mv $g $f
+        exit 1
+    fi
 done
