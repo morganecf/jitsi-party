f77386483c42297088fddcea0af1be9c92b87dc6
Andrew Hall <hndrewaall@gmail.com>
2022-05-03 16:07:08 -0400

    Temple 5/3/22

--- authbot/authbot.py
+++ authbot/authbot.py
@@ -17,8 +17,8 @@ from slack_sdk.errors import SlackApiError
 
 SYNC_MAP = {
     "all-holidays-ticket-2022-pass": "AllHolidays2022",
-    "hexennacht-at-the-estate-04-28": "Hex2022",
     "spring-2022-benefactors-support-the-satanic-estate": "Benefactors",
+    "temple-5-3-22-toxic-masculinity": "Temple3May22",
 }
 
 
